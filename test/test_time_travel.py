dbf7688936430f0e50ea725ccc6209cba6d237b5
Matt Campbell <calvinmc@gmail.com>
2019-09-16 12:40:30 -0400

    Add a bunch of callback execution tests

--- test/test_time_travel.py
+++ test/test_time_travel.py
@@ -61,7 +61,7 @@ class Test_fast_forward:
         assert automation_at_noon.datetime() == datetime.datetime(2010, 1, 1, 15, 00)
 
 
-class Test_run_in:
+class Test_callback_execution:
     def test_callbacks_are_run_in_time_order(self, time_travel, automation):
         first_mock = mock.Mock()
         second_mock = mock.Mock()
@@ -80,26 +80,49 @@ class Test_run_in:
         expected_call_order = [mock.call.first_mock({}), mock.call.second_mock({}), mock.call.third_mock({})]
         assert manager.mock_calls == expected_call_order
 
-def test_callback_not_called_before_timeout(time_travel, automation):
-    foo = mock.Mock()
-    automation.run_in(foo, 10)
-    time_travel.fast_forward(5).seconds()
-    foo.assert_not_called()
+    def test_callback_not_called_before_timeout(self, time_travel, automation):
+        callback_mock = mock.Mock()
+        automation.run_in(callback_mock, 10)
+        time_travel.fast_forward(5).seconds()
+        callback_mock.assert_not_called()
 
+    def test_callback_called_after_timeout(self, time_travel, automation):
+        scheduled_callback = mock.Mock(name="Scheduled Callback")
+        automation.run_in(scheduled_callback, 10)
+        time_travel.fast_forward(20).seconds()
+        scheduled_callback.assert_called()
 
-def test_callback_called_after_timeout(time_travel, automation):
-    scheduled_callback = mock.Mock(name="Scheduled Callback")
-    automation.run_in(scheduled_callback, 10)
-    time_travel.fast_forward(20).seconds()
-    scheduled_callback.assert_called()
+    def test_canceled_timer_does_not_run_callback(self, time_travel, automation):
+        callback_mock = mock.Mock()
+        handle = automation.run_in(callback_mock, 10)
+        time_travel.fast_forward(5).seconds()
+        automation.cancel_timer(handle)
+        time_travel.fast_forward(10).seconds()
+        callback_mock.assert_not_called()
 
+    def test_time_is_correct_when_callback_it_run(self, time_travel, automation):
+        time_travel.reset_time(datetime.datetime(2010, 1, 1, 12, 0))
 
-def test_canceled_timer_does_not_run_callback(time_travel, automation):
-    foo = mock.Mock()
-    handle = automation.run_in(foo, 10)
-    time_travel.fast_forward(5).seconds()
-    automation.cancel_timer(handle)
-    time_travel.fast_forward(10).seconds()
-    foo.assert_not_called()
+        time_when_called = []
+        def callback(kwargs):
+            nonlocal time_when_called
+            time_when_called.append(automation.datetime())
 
-# Make sure they are called in time order
+        automation.run_in(callback, 1)
+        automation.run_in(callback, 15)
+        automation.run_in(callback, 65)
+        time_travel.fast_forward(90).seconds()
+
+        expected_call_times = [
+            datetime.datetime(2010, 1, 1, 12, 0, 1),
+            datetime.datetime(2010, 1, 1, 12, 0, 15),
+            datetime.datetime(2010, 1, 1, 12, 1, 5),
+        ]
+        assert expected_call_times == time_when_called
+
+    def test_callback_called_with_correct_args(self, time_travel, automation):
+        pass
+
+class Test_reset_time:
+    def test_throws_exception_when_reset_time_is_called_with_registed_callbacks(self):
+        pass
